3243ccad4888a8afb451b9abdc0ace806a71a799
kkkkyrie <2323740041@qq.com>
2022-04-23 21:04:54 +0800

    show

---
+++ query/vue.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  outputDir: 'dist', //build输出⽬录
+  assetsDir: 'assets', //静态资源⽬录（js, css, img）
+  lintOnSave: false, //是否开启eslint
+  devServer: {
+    open: true, //是否⾃动弹出浏览器⻚⾯
+    port: '8081',
+    https: false, //是否使⽤https协议
+    hotOnly: false, //是否开启热更新
+    proxy: {
+      '/api': {
+        target: 'http://192.168.43.27:8091', //API服务器的地址
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '' // 重写路径，因为后端没有/api，所以在识别的时候会把/api替换成''
+        }
+      }
+      // '/query': 'http://localhost:8091'
+    },
+  }
+}
+ 
